1bf216516985c2b0b731aa78ef68c771209e4460
ThijsDollekens <87063168+ThijsDollekens@users.noreply.github.com>
2021-08-01 15:44:07 +0200

    Add files via upload

---
+++ bar.js
@@ -0,0 +1,23 @@
+const leeftijd = 18
+
+if (leeftijd > 17.9) {
+    console.log("U bent oud genoeg en mag de bar in");
+} else {
+    console.log("Je bent helaas nog te jong")
+}
+
+const isFemale = true
+
+if (isFemale) {
+    console.log("U bent een vrouw en mag de bar in");
+} else {
+    console.log("Alleen vrouwen mogen de bar in")
+}
+
+const driverStatus = 'bob'
+
+if (driverStatus == 'bob') {
+    console.log("U mag naar huis rijden");
+} else {
+    console.log("u mag zelf niet naar huis rijden")
+}
